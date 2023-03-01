30ec8275e61b9790685570758d4d60eb1900ad41
unknowntrojan <14975032+unknowntrojan@users.noreply.github.com>
2023-03-01 23:22:48 +0100

    added a missing parameter in example

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -28,7 +28,7 @@ use windows::{
 };
 
 #[no_mangle]
-extern "stdcall" fn DllMain(hinst: usize, reason: u32) -> i32 {
+extern "stdcall" fn DllMain(hinst: usize, reason: u32, _reserved: *mut ()) -> i32 {
     if reason == 1 {
         std::thread::spawn(move || unsafe { main_thread(hinst) });
     }
